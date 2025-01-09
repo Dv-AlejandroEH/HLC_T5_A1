9b38ce99c77920d7736da59549ab5d11fda1271e
Dv-AlejandroEH <aespher121@gmail.com>
2025-01-09 08:50:10 +0100

    Act 1 & 2 finished

---
+++ ejercicio2.py
@@ -0,0 +1,4 @@
+numero1 = int(input("Introduzca el primer número: "))
+numero2 = int(input("Introduzca el segundo número: "))
+resultado = numero1 + numero2
+print("La suma de", numero1, "y", numero2, "es igual a: ", resultado)
