71acf0ce6f5dd77d6611bc96b2eef545516ec22f
Dv-AlejandroEH <aespher121@gmail.com>
2025-01-09 09:24:05 +0100

    Act 6 finished

---
+++ ejercicio6.py
@@ -0,0 +1,8 @@
+numero1 = int(input("Introduzca el primer número a comparar: "))
+numero2 = int(input("Introduzca el segundo número a comparar: "))
+if numero1>numero2:
+    print("El número", numero1, "es mayor que el número", numero2)
+if numero1<numero2:
+    print("El número", numero1, "es menor que el número", numero2)
+if numero1==numero2:
+    print("Los números son iguales")
