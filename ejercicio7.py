e8c4d925ac8142b04927131df991cbf4476a604f
Dv-AlejandroEH <aespher121@gmail.com>
2025-01-10 11:52:18 +0100

    Act 7 finished

---
+++ ejercicio7.py
@@ -0,0 +1,6 @@
+def suma(num1, num2):
+    return num1+num2
+numero_1 = int(input("Introduzca el primer número: "))
+numero_2 = int(input("Introduzca el primer número: "))
+resultado = suma(numero_1, numero_2)
+print("El resultado de la suma es:", resultado)
